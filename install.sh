fd29af74433830778eef1eabc75e371b618a35c3
Stéphane Este-Gracias <sestegra@gmail.com>
2021-01-06 16:17:50 +0100

    Factorize install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ORG=humansriot
 HELPER=kind
-INSTALL_PATH="$HOME"/.humansriot/$HELPER
+INSTALL_PATH="$HOME"/.$ORG/$HELPER
 
 # Clone repository
 rm -rf "$INSTALL_PATH"
 mkdir -p "$INSTALL_PATH"
 cd "$(dirname "$INSTALL_PATH")"
-git clone git@github.com:humansriot/$HELPER.git
+git clone git@github.com:$ORG/$HELPER.git
 
 # Link tools
 ln -sf "$INSTALL_PATH"/kind_helper.sh /usr/local/bin/kind_helper
